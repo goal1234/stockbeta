26daaaf87e5335b4d9b620ac03a6eea495ad3562
goal1234 <946883098@qq.com>
2018-10-05 07:54:35 +0800

    industry_beta

--- basic/basic.R
+++ basic/basic.R
@@ -1,11 +1,5 @@
 
-require(fBasics)
-require(dplyr)
-qret <- test %>% group_by(code) %>%arrange(date) %>% mutate(changepercent = returns(close)*100)
 
-qret <- qret %>% mutate(y = year(date), m = month(date), d = day(date), q = quarter(date), w = week(date))
-
-qret <- na.omit(qret)
 
 me <- qret %>% group_by(c_name,y) %>% summarise(avg = mean(changepercent))
 
--- basic/ridge_beta.R
+++ basic/ridge_beta.R
@@ -5,6 +5,8 @@ require(ridge)
 require(lubridate)
 require(fBasics)
 
+
+# 对于所有的股票而言的
 index <- test %>% group_by(date) %>% 
   summarize(wei_vol = weighted.mean(close, volume)) %>% 
   mutate(change = returns(wei_vol)*100) %>% na.omit(.) 
@@ -25,4 +27,72 @@ beta_rq <-
   p %>% do(mod = rq(change~changepercent +0, data = .)) %>% 
     mutate(beta = coef(mod)[1]) %>% select(-mod)
 
+
+
+#################### 
+#对于行业进行
+
+index <- test %>% group_by(c_name, date) %>% 
+  summarize(wei_vol = weighted.mean(close, volume)) %>% 
+  mutate(change = returns(wei_vol)*100) %>% na.omit(.) 
+
+
+pre_beta <- left_join(qret, index, by = c("c_name","date")) %>% arrange(code, date)
+pre_beta <- na.omit(pre_beta)
+
+p <- pre_beta %>% group_by(c_name, code) 
+
+
+
+beta_lm <- p %>% do(mod = lm(change~changepercent +0, data = .)) %>% 
+  mutate(beta = coef(mod)[1]) %>% select(-mod)
+
+require(quantreg) #奇异矩阵 rq nlrq失败
+require(MASS)#rlm失败
+require(gbm)
+beta_rq <- 
+  p %>% do(mod = gbm(change~changepercent + 0, data = ., distribution = "laplace")) %>% 
+  mutate(beta = coef(mod)[1]) %>% select(-mod)
+
+
+A <- p %>% mutate(n = n())
+
+require(xgboost)
+beta_rq <- 
+  p %>% do(mod = gbm(change~changepercent + 0, data = ., distribution = "laplace")) %>% 
+  mutate(beta = coef(mod)[1]) %>% select(-mod)
+
+
+##########################################
+# 按照行业季度
+
+index <- qret %>% group_by(c_name, q, date) %>% 
+  summarize(wei_vol = weighted.mean(close, volume)) %>% 
+  mutate(change = returns(wei_vol)*100) %>% na.omit(.) 
+
+
+pre_beta <- left_join(qret, index, by = c("c_name", "q", "date")) %>% arrange(code, date)
+pre_beta <- na.omit(pre_beta)
+
+p <- pre_beta %>% group_by(c_name, q, code) 
+
+
+
+beta_lm <- p %>% do(mod = lm(change~changepercent +0, data = .)) %>% 
+  mutate(beta = coef(mod)[1]) %>% select(-mod)
+
+require(quantreg) #奇异矩阵 rq nlrq失败
+require(MASS)#rlm失败
+require(gbm)
+beta_rq <- 
+  p %>% do(mod = rq(change~changepercent + 0, data = .)) %>% 
+  mutate(beta = coef(mod)[1]) %>% select(-mod)
+
+
+A <- p %>% mutate(n = n())
+
+require(xgboost)
+beta_rq <- 
+  p %>% do(mod = gbm(change~changepercent + 0, data = ., distribution = "laplace")) %>% 
+  mutate(beta = coef(mod)[1]) %>% select(-mod)
 
